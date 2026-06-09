2f9d9a64ad1e265d95811a9dd5261ea9ad0bac6a
lalettywambo <lalettywambo@gmail.com>
2026-06-09 11:49:41 +0300

    add content in files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(`Hello love love you`)
+console.log(`Hello love love you`) // to add content
+
+window.alert(`This is an alert`) //to add a window
+
+
+//to add content on html file using js
+
+document.getElementById("my h1").textContent = 'HELLO';
+document.getElementById("my p").textContent = 'Hi guys just trying things out... they are pretty cool';
